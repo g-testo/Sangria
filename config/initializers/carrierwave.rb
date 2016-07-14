9003a2b71848d720b70da59a9c209b6c2dd20e0e
Jordan <jordanmmanley@gmail.com>
2016-07-14 18:08:45 +0000

    added css to home, signup, login and about pages

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,13 @@
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:      ENV["AWS_ACCESS_KEY"] ,                        # required
-    aws_secret_access_key:  ENV["AWS_SECRET_KEY"] ,                        # required
-    region:                'us-east-1',                  # optional, defaults to 'us-east-1'
-  }
-  config.fog_directory  = 'sangriawebsite'                          # required
 
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'                        # required
+    config.fog_credentials = {
+      provider:              'AWS',                        # required
+      aws_access_key_id:      ENV["AWS_ACCESS_KEY"] ,                        # required
+      aws_secret_access_key:  ENV["AWS_SECRET_KEY"] ,                        # required
+      region:                'us-east-1',                  # optional, defaults to 'us-east-1'
+    }
+    config.fog_directory  = 'sangriawebsite'                          # required
+  end
 end
