f2cde09aca9dab9b473477c9dedaa601ad5b16fe
Jordan <jordanmmanley@gmail.com>
2016-07-15 23:38:57 +0000

    fixed console issue and added form css

--- app/uploaders/recipe_image_uploader.rb
+++ app/uploaders/recipe_image_uploader.rb
@@ -19,11 +19,21 @@ class RecipeImageUploader < CarrierWave::Uploader::Base
   def cache_dir
     "#{Rails.root}/tmp/uploads"
   end
+  
+  
+  
+  
+  def default_url()
+      ActionController::Base.helpers.asset_path([version_name, "default.jpg"].compact.join('_'))
+  end
+
+  
+  
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
+  
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
