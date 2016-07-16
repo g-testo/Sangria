7adcf6f4fee2d69ec6750b5884033c53ba5f754c
Jordan <jordanmmanley@gmail.com>
2016-07-16 07:00:32 +0000

    Styling index page

--- app/uploaders/recipe_image_uploader.rb
+++ app/uploaders/recipe_image_uploader.rb
@@ -17,19 +17,17 @@ class RecipeImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    ""
   end
 
   def cache_dir
     "#{Rails.root}/tmp/uploads"
   end
   
-  def default_url()
-      ActionController::Base.helpers.asset_path([version_name, "default.jpg"].compact.join('_'))
+  def default_url(*args)
+    ActionController::Base.helpers.asset_path([version_name, "default.jpg"].compact.join('_'))
   end
 
-  
-  
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
