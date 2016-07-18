0d3260ec9bafd93b9cc0ce952fc34ab6302167c8
Jordan <jordanmmanley@gmail.com>
2016-07-18 23:22:44 +0000

    fixed carrierwave!

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,5 +2,14 @@ class Recipe < ActiveRecord::Base
     mount_uploader :recipe_image, RecipeImageUploader
     # validates_presence_of :author, :instructions, :servings
     # validates :name, presence: true, uniqueness: true, length: { minimum: 5, maximum: 80 }
+    validate :recipe_image_size
     
+    
+    private
+    
+    def recipe_image_size
+      if recipe_image.size > 5.megabytes
+        errors.add(:recipe_image, "should be less than 5MB")
+      end
+    end
 end
