09b9bbb2587d0e87aea2676637fc3d9166afb246
Jordan <jordanmmanley@gmail.com>
2016-07-28 04:46:28 +0000

    Yelp api is setup on home page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -43,6 +43,6 @@ class RecipesController < ApplicationController
   
   private
     def recipe_params
-      params.require(:recipe).permit(:name, :instructions, :author, :servings, :recipe_image)
+      params.require(:recipe).permit(:name, :instructions, :author, :servings, :recipe_image, :user_id)
     end
 end
