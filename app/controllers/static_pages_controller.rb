ab5de6fad22f9d98e5f8b187f39ad3bb8a24d39c
Jordan <jordanmmanley@gmail.com>
2016-07-31 01:31:18 +0000

    clean up flikr api call

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,6 +7,15 @@ class StaticPagesController < ApplicationController
       config.access_token_secret = ENV["YOUR_ACCESS_SECRET"]
     end
     
+    flikr_response = flickr.photos.search(:tags => "sangria", :per_page => 6)
+    
+    @flikr_photos = []
+    
+    flikr_response.each do |image| 
+      id = flickr.photos.getInfo(:photo_id => image.id)
+      photo = FlickRaw.url_t(id) 
+      @flikr_photos.push(photo)
+    end 
     
     @yelp_response = Yelp.client.search('New York City', { term: 'Sangria Bars', limit: '10' })
     
