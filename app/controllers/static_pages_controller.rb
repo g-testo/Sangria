6829f1a4bca8af7f4bab85ba5282a1103f5e5bcd
Jordan <jordanmmanley@gmail.com>
2016-08-20 18:53:17 +0000

    changed home page layout and add youtube video

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,20 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
-    @twitter_response = ApplicationController::CLIENT.search("#SangriaNYC -rt", lang: "en").take(5)
-    
-    flikr_response = flickr.photos.search(:tags => "sangria", :per_page => 6)
-    
-    @flickr_photos = []
-    
-    flikr_response.each do |image| 
-      id = flickr.photos.getInfo(:photo_id => image.id)
-      photo = FlickRaw.url_t(id) 
-      @flickr_photos.push(photo)
-    end 
-    
+    # @twitter_response = ApplicationController::CLIENT.search("#SangriaNYC -rt", lang: "en").take(5)
     @yelp_response = Yelp.client.search('New York City', { term: 'Sangria Bars', limit: '10' })
-    
-    
   end
   def about
   end
