2a65d7ffe09d7aea608ce1e0f969abeaf80f5123
Jordan <jordanmmanley@gmail.com>
2016-08-24 14:09:07 +0000

    home view css changes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     # @twitter_response = ApplicationController::CLIENT.search("#SangriaNYC -rt", lang: "en").take(5)
-    @yelp_response = Yelp.client.search('New York City', { term: 'Sangria Bars', limit: '5' })
+    @yelp_response = Yelp.client.search('New York City', { term: 'Sangria Bars', limit: '6' })
   end
   def about
   end
