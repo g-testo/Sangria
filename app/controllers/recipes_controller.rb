1a8fd186cde6b0d8a391e84fb8052a19517162ad
Gregory Testo <cryptomonger@hotmail.com>
2017-07-20 13:30:14 -0700

    fixed recipe submit issue

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,40 +12,42 @@ class RecipesController < ApplicationController
     @recipe = current_user.recipes.new
   end
 
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
   def create
     @recipe = current_user.recipes.new(recipe_params)
-    if @recipe.save
+    current_user.inspect
+    @recipe.author = current_user.user_name
+    if @recipe.save!
         redirect_to @recipe, notice: "Recipe created successfully."
     else
         redirect_to new_recipe_path, alert: "Error creating recipe."
     end
   end
 
-  def show
-    @recipe = Recipe.find(params[:id])
-  end
-  
-  def edit
-    @recipe = Recipe.find(params[:id])
-  end
-  
   def update
     @recipe = Recipe.find(params[:id])
-    
+
     if @recipe.update(recipe_params)
         redirect_to @recipe, notice: "Recipe updated successfully."
     else
         redirect_to edit_recipe_path(@recipe), alert: "Error updating recipe."
     end
   end
-  
+
   def destroy
     @recipe = Recipe.find(params[:id])
     @recipe.destroy
-    
+
     redirect_to recipes_path
   end
-  
+
   private
     def recipe_params
       params.require(:recipe).permit(:name, :instructions, :author, :servings, :recipe_image, :user_id, :flavor, :ingredients)
