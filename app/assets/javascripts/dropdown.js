92c6b4f1f82cbdec537fce8964ef867230ea8cb3
Gregory Testo <cryptomonger@hotmail.com>
2017-07-22 15:51:20 -0700

    added toggle to make sure only one nav drop down is active at a time

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,17 +1,29 @@
 function dropdownFunction(dropdownList) {
-    if (dropdownList == "flavor"){
-      document.getElementById("flavorDropdown").classList.toggle("show");
-    } else if (dropdownList == "account"){
-      document.getElementById("accountDropdown").classList.toggle("show");
-    }
+  if (dropdownList == "flavor"){
+    document.getElementById("accountDropdown").classList.remove("show");
+    document.getElementById("flavorDropdown").classList.toggle("show");
+  } else if (dropdownList == "account"){
+    document.getElementById("flavorDropdown").classList.remove("show");
+    document.getElementById("accountDropdown").classList.toggle("show");
+  }
 }
 
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+  // openDropdownList = document.querySelectorAll('[id$="Dropdown"]');
+//   for (var i=0;i<openDropdownList.length;i++){
+    // if (openDropdownList[i].classList.contains('show')) {
+//       console.log("True")
+//       // document.getElementById(openDropdownList[i].id).classList.remove('show')
+//     // document.getElementsByClassName(openDropdownList[i]).classList.remove('show');
+//     // console.log(openDropdownList[i].id);
+//   //   if (openDropdownList[i].classList.contains('show')) {
+//   //   }
+//   }
+// }
 
+  if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
+    for (var i=0; i<dropdowns.length;i++) {
       var openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
