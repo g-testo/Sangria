86735cb86a5956f03f64469f82ee6524ff5e0d90
Gregory Testo <cryptomonger@hotmail.com>
2017-07-22 13:53:58 -0700

    added nav recipe sorting dropdown shortcut

---
+++ app/assets/javascripts/dropdown.js
@@ -0,0 +1,21 @@
+function dropdownFunction(dropdownList) {
+    if (dropdownList == "flavor"){
+      document.getElementById("flavorDropdown").classList.toggle("show");
+    } else if (dropdownList == "account"){
+      document.getElementById("accountDropdown").classList.toggle("show");
+    }
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
