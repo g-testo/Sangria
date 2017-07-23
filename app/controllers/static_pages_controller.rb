71e879a80f8aae3f2ec406d7546100b0374effd7
Gregory Testo <cryptomonger@hotmail.com>
2017-07-23 09:51:33 -0700

    reset data to push to heroku and fixed recipe_roulette not showing up when noting in db

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
     @yelp_response = Yelp.client.search('New York City', { term: 'Sangria Bars', limit: '6' })
+    if Recipe.ids.length != 0
+      @randomRecipe = Recipe.find(Recipe.ids.sample)
+    end
   end
   def about
   end
