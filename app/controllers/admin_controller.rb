a590e2d6cd2a01cb3e92ab3182ac8f4da432f876
Gregory Testo <cryptomonger@hotmail.com>
2017-07-30 00:09:38 -0700

    added limited access to admin page from address bar to only admins

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,11 @@
 class AdminController < ActionController::Base
+  before_filter :authenticate
 
   def admin_page
     @recipes = Recipe.all
     @users = User.all
+  end
+  def authenticate
+    redirect_to(root_path) unless current_user.admin?
   end
 end
