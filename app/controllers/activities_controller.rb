b60ae828c8f79cae0dc8415552509cc55e8e7550
Gregory Testo <cryptomonger@hotmail.com>
2017-08-11 05:39:03 -0700

    feed now shows activites of following users

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,5 +2,6 @@ class ActivitiesController < ApplicationController
   def index
       @activities = PublicActivity::Activity.order("created_at desc")
       @users = User.where(is_private = false).order(created_at: :desc).all
+      @following_activities = PublicActivity::Activity.order("created_at desc").where(owner_type: "User", owner_id: current_user.following.map {|u| u.id}).all
   end
 end
