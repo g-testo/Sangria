d6827c3266b6aeacf436e4473e4ef208eb852bab
Gregory Testo <cryptomonger@hotmail.com>
2017-08-17 06:27:17 -0700

    edited message for activity feed

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -107,6 +107,6 @@ class RecipesController < ApplicationController
   end
 
   def recipe_params
-    params.require(:recipe).permit(:name, :description, :instructions, :author, :servings, :recipe_image, :user_id, :flavor, :avg_rating, ingredients_attributes: [:name, :quantity, :category, :_destroy, :id])
+    params.require(:recipe).permit(:name, :description, :instructions, :author, :servings, :recipe_image, :user_id, :flavor, :avg_rating, ingredients_attributes: [:name, :quantity, :category, :_destroy, :id, :recipe_id])
   end
 end
